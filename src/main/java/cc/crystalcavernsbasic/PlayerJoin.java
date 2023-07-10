df2f94b9cafb59541748a8f1b00130ecbe2d3b84
Jiří Sitera <78226565+jirisitera@users.noreply.github.com>
2023-07-10 22:26:12 +0200

    Join fade effect

--- src/main/java/cc/crystalcavernsbasic/PlayerJoin.java
+++ src/main/java/cc/crystalcavernsbasic/PlayerJoin.java
@@ -1,5 +1,6 @@
 package cc.crystalcavernsbasic;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,6 +10,8 @@ import static cc.crystalcavernsbasic.CrystalCavernsBasic.toSend;
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        toSend.add(e.getPlayer().getUniqueId());
+        Player p = e.getPlayer();
+        toSend.add(p.getUniqueId());
+        p.sendTitle("\uDBEA\uDDE8", "", 0, 10, 10);
     }
 }
