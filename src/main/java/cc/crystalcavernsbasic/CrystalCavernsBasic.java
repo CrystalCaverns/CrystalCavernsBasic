032e8572d9639f8fd42eac38a72df16dc3f8a06f
Jiří Sitera <78226565+jirisitera@users.noreply.github.com>
2023-08-23 16:27:43 +0200

    Brought up to standard with lobby plugin

--- src/main/java/cc/crystalcavernsbasic/CrystalCavernsBasic.java
+++ src/main/java/cc/crystalcavernsbasic/CrystalCavernsBasic.java
@@ -1,11 +1,5 @@
 package cc.crystalcavernsbasic;
 
-import de.themoep.minedown.MineDown;
-import me.clip.placeholderapi.PlaceholderAPI;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -17,20 +11,10 @@ public final class CrystalCavernsBasic extends JavaPlugin {
     public static List<UUID> toSend = new ArrayList<>();
     @Override
     public void onEnable() {
-        Objects.requireNonNull(getCommand("profile")).setExecutor(new cc.crystalcavernsbasic.ProfileCommand());
+        plugin = this;
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+        Objects.requireNonNull(getCommand("spawn")).setExecutor(new SpawnCommand());
         getLogger().info("Crystal Caverns Basic plugin loaded successfully!");
-        Bukkit.getScheduler().runTaskTimer(this, () -> {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                if (toSend.contains(player.getUniqueId())) {
-                    String message = "&#bbbbbb&%vault_eco_balance% &white&\uDBEB\uDEEA      &#9944ff&%gamepoints_balance% &white&\uDBCB\uDD10";
-                    message = PlaceholderAPI.setPlaceholders(player, message);
-                    TextComponent formatted_message = new TextComponent(MineDown.parse(message));
-                    formatted_message.setFont("currency_display");
-                    player.sendMessage(ChatMessageType.ACTION_BAR,formatted_message);
-                }
-            }
-        }, 30L, 30L);
     }
+    public static JavaPlugin plugin;
 }
-
